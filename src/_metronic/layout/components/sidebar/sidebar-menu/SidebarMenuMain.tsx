80081185cc6bb39851dea8add74fbf4b91d0c217
rawanalraiisi <rawanalraiisi122@gmail.com>
2023-09-12 20:42:59 +0400

    update

--- src/_metronic/layout/components/sidebar/sidebar-menu/SidebarMenuMain.tsx
+++ src/_metronic/layout/components/sidebar/sidebar-menu/SidebarMenuMain.tsx
@@ -279,7 +279,7 @@ const SidebarMenuMain = () => {
           
 
       
-       
+{/*        
       <div className='menu-item'>
         <div className='menu-content pt-8 pb-2'>
           <span className='menu-section text-muted text-uppercase fs-8 ls-1'>Crafted</span>
@@ -379,7 +379,7 @@ const SidebarMenuMain = () => {
           </span>
           <span className='menu-title'>Changelog {process.env.REACT_APP_VERSION}</span>
         </a>
-      </div>
+      </div> */}
 
 <br></br><br></br><br></br><br></br><br></br>
     </>
